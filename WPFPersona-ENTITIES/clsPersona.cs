f8674cde0d24d1f08602c432aac82dc31d9506bc
dbenitez <dbenitez@107-14>
2016-12-12 12:15:38 +0100

    asildfhasdhlae

---
+++ WPFPersona-ENTITIES/clsPersona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPersona_ENTITIES
+{
+    public class clsPersona
+    {
+        //Atributos
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+        public DateTime FechaNac { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+
+        //Constructores
+        public clsPersona()
+        {
+            Id = 1;
+            Nombre = "";
+            Apellidos = "";
+            FechaNac = new DateTime();
+            Direccion = "";
+            Telefono = "";
+        }
+
+        public clsPersona(int id, String nombre, String apellidos, DateTime fechaNac, String direccion, String telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellidos = apellidos;
+            FechaNac = fechaNac;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+    }
+}
